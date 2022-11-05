1fe26e782c117a8c6023167511904ae2b25d66ea
eric <fantasy_2003_@hotmail.com>
2022-11-05 08:57:46 +0800

    add chapter8 01

---
+++ codes/Kaleidoscope/chapter8/testobj/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+extern "C" {
+    double average(double, double);
+}
+
+int main() {
+    std::cout << "average of 3.0 and 4.0: " << average(3.0, 4.0) << std::endl;
+    return 0;
+}
